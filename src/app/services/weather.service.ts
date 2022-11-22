185091fcbc8b8102eea6f1c9cc377eabbe72bc50
Kishan-Kumar-Zalavadia <kishankumar3131@gmail.com>
2022-11-22 15:20:17 +0530

    init

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -12,20 +12,20 @@ export class WeatherService {
   constructor(private http:HttpClient) {   }
   
 
-  getWeatherData(cityName: string):Observable<WeatherData>{
-      return this.http.get<WeatherData>(environment.weatherApiBaseUrl,{
-        params: new HttpParams()
-        .set('q',cityName)
-        .set(environment.apiName,environment.apiKey)
-        .set('units','metric')
-      })
-    }
+  // getWeatherData(cityName: string):Observable<WeatherData>{
+  //     return this.http.get<WeatherData>(environment.weatherApiBaseUrl,{
+  //       params: new HttpParams()
+  //       .set('q',cityName)
+  //       .set(environment.apiName,environment.apiKey)
+  //       .set('units','metric')
+  //     })
+  //   }
 
   // Also Be written as:
 
-  // getWeatherData(cityName: string):Observable<WeatherData>{
-  //   return this.http.get<WeatherData>('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=a8cb291f59be9ebda3db53af1804d14e&units=metric')
-  // }
+  getWeatherData(cityName: string):Observable<WeatherData>{
+    return this.http.get<WeatherData>('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=a8cb291f59be9ebda3db53af1804d14e&units=metric')
+  }
 
     
 
